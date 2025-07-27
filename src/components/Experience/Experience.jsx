8ea9127e628c6afaf9631f88edc0c8369eac6218
Suhani411 <suhani4shinde@gmail.com>
2025-07-27 13:53:28 +0530

    images added

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
     <section className={styles.container} id="experience">
         <h2 className={styles.title} >Experience</h2>
         <div className={styles.content}>
-            <div className={styles.container}>{
+            <div className={styles.skills}>{
                 skills.map((skill, id) => {
                     return <div key={id} className={styles.skill}>
                         <div className={styles.skillImageContainer}>
